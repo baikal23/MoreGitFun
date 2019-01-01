b710e5b917bb51161004879f8be5a37348ce9b95
Sue Kohler <skohler00@gmail.com>
2018-12-31 19:44:40 -0500

    third commit

--- MoreGitFun/ViewController.swift
+++ MoreGitFun/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Original code")
         print("Second commit")
+        print("Third commit")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
